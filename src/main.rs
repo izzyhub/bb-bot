b824d0e2a3edd094541d7dbac3dd782460f30adc
Izzy <izzy@izzywatson.com>
2025-01-28 18:46:05 -0500

    OTA updating largely works.
    Commiting to save work. A lot of clean up is necessary. For instance, need to know we've just come from an update and do some checks before marking the new build good, etc.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use embedded_svc::{
     wifi::{ClientConfiguration, Configuration as WifiConfiguration},
 };
 
+use esp_idf_hal::reset;
 use esp_idf_hal::gpio;
 use esp_idf_hal::prelude::Peripherals;
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
-    hal::peripheral,
+    hal::{peripheral, task::{block_on}},
     nvs,
     ota,
     http::client::{Configuration as HttpConfiguration, EspHttpConnection, Method},
@@ -20,8 +21,9 @@ use esp_idf_svc::{
 use esp_idf_sys as _;
 use esp_idf_sys::{esp, esp_app_desc, EspError};
 use log::info;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, TcpStream};
+use log::error;
+//use tokio::io::{AsyncReadExt, AsyncWriteExt};
+//use tokio::net::{TcpListener, TcpStream};
 //use botifactory_common::{Botifactory, Identifier, NewRelease};
 use bytes::{BufMut, BytesMut};
 
@@ -37,6 +39,8 @@ use serde_json::from_slice;
 use thiserror::Error;
 
 use versions::Versioning;
+
+use tokio::time::{sleep, Duration};
 
 #[derive(Error, Debug)]
 pub enum NetworkError {
@@ -57,105 +61,54 @@ where
     let status = response.status();
     println!("status: {status}");
     let mut valid_utf8 = BytesMut::with_capacity(1024);
-    match status {
-        200..=299 => {
-            //let mut buf = BytesMut::with_capacity(1024);
-            let mut buf = [0_u8; 256];
-            let mut reader = response;
-            let mut offset = 0;
-            let mut total = 0;
-            println!("offset: {offset}");
-            while let Ok(size) = Read::read(&mut reader, &mut buf[offset..]) {
-                println!("size: {size}");
-                println!("offset: {offset}");
-                if size == 0 {
-                    // It might be nice to check if we have any left over bytes here (ie. the offset > 0)
-                    // as this would mean that the response ended with an invalid UTF-8 sequence, but for the
-                    // purposes of this training we are assuming that the full response will be valid UTF-8
-                    println!("breaking");
-                    break;
-                }
-                total += size;
-                println!("total: {total}");
+    //let mut buf = BytesMut::with_capacity(1024);
+    let mut buf = [0_u8; 256];
+    let mut reader = response;
+    let mut offset = 0;
+    let mut total = 0;
+    println!("offset: {offset}");
+    while let Ok(size) = Read::read(&mut reader, &mut buf[offset..]) {
+        println!("size: {size}");
+        println!("offset: {offset}");
+        if size == 0 {
+            // It might be nice to check if we have any left over bytes here (ie. the offset > 0)
+            // as this would mean that the response ended with an invalid UTF-8 sequence, but for the
+            // purposes of this training we are assuming that the full response will be valid UTF-8
+            println!("breaking");
+            break;
+        }
+        total += size;
+        println!("total: {total}");
 
-                let size_plus_offset = size + offset;
-                match str::from_utf8(&buf[..size_plus_offset]) {
-                    Ok(text) => {
-                        println!("text: {text}");
-                        valid_utf8.extend_from_slice(&buf[..size_plus_offset]);
-                        offset = 0;
-                    }
-                    Err(error) => {
-                        println!("error: {error}");
-                        let valid_up_to = error.valid_up_to();
-                        unsafe {
-                            println!("{}", str::from_utf8_unchecked(&buf[..valid_up_to]));
-                        }
-                        valid_utf8.extend_from_slice(&buf[..valid_up_to]);
-                        offset = size_plus_offset - valid_up_to;
-                    }
+        let size_plus_offset = size + offset;
+        match str::from_utf8(&buf[..size_plus_offset]) {
+            Ok(text) => {
+                println!("text: {text}");
+                valid_utf8.extend_from_slice(&buf[..size_plus_offset]);
+                offset = 0;
+            }
+            Err(error) => {
+                error!("error: {error}");
+                println!("error: {error}");
+                let valid_up_to = error.valid_up_to();
+                unsafe {
+                    println!("{}", str::from_utf8_unchecked(&buf[..valid_up_to]));
                 }
+                valid_utf8.extend_from_slice(&buf[..valid_up_to]);
+                offset = size_plus_offset - valid_up_to;
             }
         }
-        _ => {
-            println!("unexpected response code: {status}");
-            return Err(NetworkError::IncompleteResponse.into());
-        }
-    };
-    //println!("Incomplete response???");
-    //return Err(NetworkError::IncompleteResponse.into());
+    }
     Ok(str::from_utf8(&valid_utf8)?.to_string())
 }
 
-/*
-fn read_response_json<'a, 'b, T, C>(response: &'a mut RequestResponse<C>) -> Result<T, NetworkError>
-where
-    T: Deserialize<'b>,
-    C: embedded_svc::http::client::Connection,
-{
-    let text_response = read_response_string(response)?;
-    Ok(serde_json::from_str::<T>(&text_response)?)
-}
-*/
-
-fn test() -> Result<()> {
+fn check_for_ota() -> Result<()> {
     info!("test");
     let connection = EspHttpConnection::new(&HttpConfiguration::default())?;
     info!("created connection");
     let mut client = Client::wrap(connection);
     info!("created client");
     let headers = [("accept", "application/json")];
-
-    /*
-    let health_url = format!("{}/health_check", config::BOTIFACTORY_URL);
-    info!("health_url: {health_url}");
-    let request = client.request(Method::Get, &health_url, &headers)?;
-    info!("requesting...");
-    let response = request.submit()?;
-    info!("responded");
-    let status = response.status();
-    println!("status: {status}");
-    */
-
-    let project_url = format!(
-        "{}/project/{}",
-        config::BOTIFACTORY_URL,
-        config::BOTIFACTORY_PROJECT_NAME
-    );
-    info!("project_url: {project_url}");
-
-    info!("log world");
-    let request = client.request(Method::Get, &project_url, &headers)?;
-    info!("requesting...");
-    let mut response = request.submit()?;
-    let response_string = read_response_string(&mut response)?;
-    println!("response_string: '{response_string}'");
-    let project_response: ProjectBody = serde_json::from_str(&response_string)?;
-    println!("serde_json::from_str finished");
-    println!("project_response: {project_response}");
-    info!("project_response: {project_response}");
-
-    info!("responded");
 
     let release_url = format!(
         "{}/{}/{}/latest",
@@ -165,41 +118,63 @@ fn test() -> Result<()> {
     );
     info!("release_url: {release_url}");
 
-    info!("log world");
     let request = client.request(Method::Get, &release_url, &headers)?;
-    info!("requesting...");
     let mut response = request.submit()?;
-    info!("responded");
     let status = response.status();
-    println!("status: {status}");
+    info!("status: {status}");
     let response_string = read_response_string(&mut response)?;
-    println!("response_string: ({response_string})");
     let release_response: ReleaseBody = serde_json::from_str(&response_string)?;
-    println!("release_response: {release_response}");
+
     let latest_version = Versioning::new(release_response.release.version).ok_or(NetworkError::BadVersion)?;
-    println!("latest version: {latest_version}");
-    info!("latest version: {latest_version}");
-
-    println!("latest_version: is_ideal: {}", latest_version.is_ideal());
-
     let binary_version = Versioning::new(config::RELEASE_VERSION).ok_or(NetworkError::BadVersion)?;
-    println!("binary version: {binary_version}");
+
+    info!("latest version: {latest_version}");
+    info!("latest_version: is_ideal: {}", latest_version.is_ideal());
+
     info!("binary version: {binary_version}");
-    println!("binary_version: is_ideal: {}", binary_version.is_ideal());
+    info!("binary_version: is_ideal: {}", binary_version.is_ideal());
 
     if latest_version > binary_version {
-        println!("creating ota_man");
-        let ota_man = ota::EspOta::new()?;
-        println!("created ota_man");
+        info!("creating ota_man");
+        let mut ota_man = ota::EspOta::new()?;
+        info!("created ota_man");
         let slot = ota_man.get_boot_slot()?;
-        println!("got slot");
-        println!("boot slot: {slot:#?}");
+        info!("got slot");
+        info!("boot slot: {slot:#?}");
 
         let slot = ota_man.get_running_slot()?;
         println!("running slot: {slot:#?}");
 
         let slot = ota_man.get_update_slot()?;
         println!("update slot: {slot:#?}");
+
+        let headers = [("accept", "application/octet-stream")];
+        let mut update_process = ota_man.initiate_update()?;
+
+        let request = client.request(Method::Get, &release_url, &headers)?;
+        let mut response = request.submit()?;
+        let status = response.status();
+        info!("status: {status}");
+        if status == 200 {
+            let mut buf = [0_u8; 256];
+            while let Ok(size) = Read::read(&mut response, &mut buf) {
+                info!("size: {size}");
+                if size == 0 {
+                    // It might be nice to check if we have any left over bytes here (ie. the offset > 0)
+                    // as this would mean that the response ended with an invalid UTF-8 sequence, but for the
+                    // purposes of this training we are assuming that the full response will be valid UTF-8
+                    println!("breaking");
+                    break;
+                }
+                update_process.write(&buf)?;
+            }
+            print!("while over??");
+            //let finished = update_process.finish()?;
+            update_process.complete();
+            info!("restarting to update");
+            reset::restart();
+        }
+
     }
     else {
         println!("running the current version");
@@ -212,6 +187,23 @@ fn test() -> Result<()> {
     println!("still here world 🥺");
     info!("still here world 🥺");
     Ok(())
+}
+
+async fn wifi_setup<'a>(
+    sysloop: EspSystemEventLoop,
+    nvs: EspDefaultNvsPartition,
+) -> Result<WifiLoop<'a>> {
+    let peripherals = Peripherals::take()?;
+    let timer = EspTaskTimerService::new()?;
+    let wifi = AsyncWifi::wrap(
+        EspWifi::new(peripherals.modem, sysloop.clone(), Some(nvs))?,
+        sysloop,
+        timer.clone(),
+    )?;
+    let mut wifi_loop = WifiLoop { wifi };
+    wifi_loop.configure().await?;
+    wifi_loop.initial_connect().await?;
+    Ok(wifi_loop)
 }
 
 esp_app_desc!();
@@ -230,42 +222,51 @@ fn main() -> anyhow::Result<()> {
     esp! { unsafe { esp_idf_sys::esp_vfs_eventfd_register(&eventd_config) } }?;
 
     info!("Setting up board...");
-    let peripherals = Peripherals::take().unwrap();
+    //let peripherals = Peripherals::take()?;
     let sysloop = EspSystemEventLoop::take()?;
-    let timer = EspTaskTimerService::new()?;
     let nvs = EspDefaultNvsPartition::take()?;
 
-    info!("Initializing Wi-Fi...");
-    let wifi = AsyncWifi::wrap(
-        EspWifi::new(peripherals.modem, sysloop.clone(), Some(nvs))?,
-        sysloop,
-        timer.clone(),
-    )?;
 
     info!("Starting async run loop");
-    tokio::runtime::Builder::new_current_thread()
+    let runtime = tokio::runtime::Builder::new_current_thread()
         .enable_all()
-        .build()?
-        .block_on(async move {
-            let mut wifi_loop = WifiLoop { wifi };
-            wifi_loop.configure().await?;
-            wifi_loop.initial_connect().await?;
+        .build()?;
 
-            //use tokio::task::JoinSet;
-            //let mut set = JoinSet::new();
-            info!("Preparing to do something???");
-            //set.spawn(test());
-            match test() {
-                Ok(_) => {
-                    println!("test success");
+    println!("creating runtime???");
+    runtime.block_on(async move {
+        println!("inside runtime???");
+        let mut wifi_loop = wifi_setup(sysloop.clone(), nvs).await.expect("Failed to setup wifi");
+        info!("Initializing Wi-Fi...");
+        println!("setup wifi??");
+        tokio::spawn(async move {
+            loop {
+                while let Err(error) = wifi_loop.stay_connected().await {
+                    error!("connection error: {error}");
+                    println!("stay connected returned an err???");
                 }
-                Err(error) => {
-                    println!("error: {error}");
-                }
-            };
-            wifi_loop.stay_connected().await
-        })?;
-
+            }
+        });
+        loop {
+            println!("looping??");
+            tokio::spawn(async {
+                info!("Preparing to do something???");
+                match check_for_ota() {
+                    Ok(_) => {
+                        println!("ota success");
+                    }
+                    Err(error) => {
+                        println!("ota error: {error}");
+                    }
+                };
+            });
+            info!("napping");
+            println!("napping");
+            sleep(Duration::from_secs(10)).await;
+            println!("woke");
+            info!("woke");
+        }
+    });
+    println!("end???");
 
     Ok(())
 }
